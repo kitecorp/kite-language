f2f1ce65ba3396a4c4d722cccc1cb62f186259d5
Tudor Pop <tudor14pop@gmail.com>
2025-06-28 21:53:12 +0300

    Introduce KiteTest and refactor testing

---
+++ src/test/java/io/zmeu/KiteTest.java
@@ -0,0 +1,21 @@
+package io.zmeu;
+
+import io.zmeu.Runtime.BaseRuntimeTest;
+import io.zmeu.TypeChecker.TypeChecker;
+import org.junit.jupiter.api.BeforeEach;
+
+public class KiteTest extends BaseRuntimeTest {
+    protected TypeChecker checker;
+
+    @BeforeEach
+    void setUp() {
+        checker = new TypeChecker();
+    }
+
+    protected Object eval(String source) {
+        program = super.src(source);        // parse
+        resolver.resolve(program);          // resolve block scopes inconsistencies
+        checker.visit(program);             // check types
+        return interpreter.visit(program);  // interpret
+    }
+}
