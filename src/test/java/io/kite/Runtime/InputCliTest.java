ee43bb40317c3ab971cee2f32ae4c652d5eb389a
Tudor Pop <tudor14pop@gmail.com>
2025-08-31 16:48:09 +0300

    Create input CLI test

--- src/test/java/io/kite/Runtime/InputCliTest.java
+++ src/test/java/io/kite/Runtime/InputCliTest.java
@@ -3,10 +3,23 @@ package io.kite.Runtime;
 import io.kite.Runtime.Inputs.CliResolver;
 import io.kite.Runtime.Inputs.InputResolver;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterEach;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 public class InputCliTest extends InputTest {
+    private InputStream sysInBackup = System.in;
+
+    @AfterEach
+    public void cleanup() {
+        System.setIn(sysInBackup);
+    }
+
+    protected void setInput(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
 
     @Override
     protected @NotNull List<InputResolver> getResolvers() {
