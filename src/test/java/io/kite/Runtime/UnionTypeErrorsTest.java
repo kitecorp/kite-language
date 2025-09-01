d9a4d0f2722d606743a35f884d37340bbddc5afa
Tudor Pop <tudor14pop@gmail.com>
2025-09-01 17:23:48 +0300

    Add boolean array type input

---
+++ src/test/java/io/kite/Runtime/UnionTypeErrorsTest.java
@@ -0,0 +1,101 @@
+package io.kite.Runtime;
+
+import io.kite.Base.RuntimeTest;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Log4j2
+public class UnionTypeErrorsTest extends RuntimeTest {
+
+    @Test
+    @DisplayName("Should throw error if assigning the wrong value to a union type of numbers")
+    void ShouldThrow() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            eval("""
+                    type customNumbers = 1 | 2 | 5
+                    var customNumbers numbers = 3
+                    """);
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw error if assigning a string to a union type of numbers")
+    void shouldThrowString() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            eval("""
+                    type customNumbers = 1 | 2 | 5
+                    var customNumbers numbers = 'hello'
+                    """);
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw error if assigning a false to a union type of numbers")
+    void shouldThrowIfAssignFalseToNumberUnionType() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            eval("""
+                    type customNumbers = 1 | 2 | 5
+                    var customNumbers numbers = false
+                    """);
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw error if assigning a true to a union type of numbers")
+    void shouldThrowIfAssignTrueToNumberUnionType() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            eval("""
+                    type customNumbers = 1 | 2 | 5
+                    var customNumbers numbers = true
+                    """);
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw error if assigning a object to a union type of numbers")
+    void shouldThrowIfAssignObjectToNumberUnionType() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            eval("""
+                    type customNumbers = 1 | 2 | 5
+                    var customNumbers numbers = { hello: 'world' }
+                    """);
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw error if assigning a array of string to a union type of numbers")
+    void shouldThrowIfAssignArrayToNumberUnionType() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            eval("""
+                    type customNumbers = 1 | 2 | 5
+                    var customNumbers numbers = ['hello']
+                    """);
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw error if assigning a array of numbers to a union type of numbers")
+    void shouldThrowIfAssignArrayOfValidNumbersToNumberUnionType() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            eval("""
+                    type customNumbers = 1 | 2 | 5
+                    var customNumbers numbers = [1,2,5]
+                    """);
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw error if array contains incompatible types")
+    void arrayOfTypeShouldThrow() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            eval("""
+                    type customNumbers = 1 | 2 | 5
+                    var customNumbers[] numbers = [3]
+                    """);
+        });
+    }
+
+}
