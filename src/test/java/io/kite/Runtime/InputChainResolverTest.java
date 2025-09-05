22ef999c84edfb8423e2c83544f03202c31b0bc6
Tudor Pop <tudor14pop@gmail.com>
2025-09-05 14:16:01 +0300

    Add some quick checks

--- src/test/java/io/kite/Runtime/InputChainResolverTest.java
+++ src/test/java/io/kite/Runtime/InputChainResolverTest.java
@@ -4,8 +4,11 @@ import io.kite.Base.RuntimeTest;
 import io.kite.Frontend.Parse.Literals.TypeIdentifier;
 import io.kite.Frontend.Parser.Expressions.InputDeclaration;
 import io.kite.Runtime.Inputs.ChainResolver;
+import io.kite.Runtime.exceptions.MissingInputException;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @Log4j2
@@ -18,7 +21,15 @@ public class InputChainResolverTest extends RuntimeTest {
     }
 
     @Test
-    void testOrder() {
+    @DisplayName("Env+File skipped throws on last step")
+    void testThrowsOnLastStepCLI() {
+        Assertions.assertThrows(MissingInputException.class, () -> chainResolver.visit(InputDeclaration.input("region", TypeIdentifier.type("string"))));
+    }
+
+    @Test
+    @DisplayName("Env is takes precedence")
+    void test() {
+
         chainResolver.visit(InputDeclaration.input("region", TypeIdentifier.type("string")));
     }
 
