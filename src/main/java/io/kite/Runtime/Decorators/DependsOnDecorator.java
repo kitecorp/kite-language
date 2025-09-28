4deae1b9a37b53768e62c7b513044dd0f860c54c
Tudor Pop <tudor14pop@gmail.com>
2025-09-28 19:41:38 +0300

    Add dependsOn single resource value

--- src/main/java/io/kite/Runtime/Decorators/DependsOnDecorator.java
+++ src/main/java/io/kite/Runtime/Decorators/DependsOnDecorator.java
@@ -1,12 +1,14 @@
 package io.kite.Runtime.Decorators;
 
 import io.kite.Frontend.Parser.Expressions.AnnotationDeclaration;
+import io.kite.Frontend.Parser.Expressions.Expression;
 import io.kite.Frontend.Parser.Expressions.MemberExpression;
 import io.kite.Frontend.Parser.Expressions.ResourceExpression;
 import io.kite.Runtime.Interpreter;
 import io.kite.Visitors.SyntaxPrinter;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class DependsOnDecorator extends NumberDecorator {
@@ -19,16 +21,24 @@ public class DependsOnDecorator extends NumberDecorator {
     @Override
     public Object execute(Interpreter interpreter, AnnotationDeclaration declaration) {
         if (declaration.getValue() instanceof MemberExpression member) {
-            return registerDependency(declaration, member);
-        } else {
+            return registerDependency(declaration, Set.of(printer.visit(member)));
+        } else if (declaration.getArgs() != null) {
+            var set = new HashSet<String>();
+            for (Expression item : declaration.getArgs().getItems()) {
+                if (item instanceof MemberExpression memberExpression) {
+                    set.add(printer.visit(memberExpression));
+                }
+            }
+            registerDependency(declaration, set);
+            return set;
         }
 
         return null;
     }
 
-    private @NotNull Set<String> registerDependency(AnnotationDeclaration declaration, MemberExpression member) {
+    private @NotNull Set<String> registerDependency(AnnotationDeclaration declaration, Set<String> visit) {
         var resource = (ResourceExpression) declaration.getTarget();
-        resource.setDependencies(Set.of(printer.visit(member)));
+        resource.setDependencies(visit);
         return resource.getDependencies();
     }
 
