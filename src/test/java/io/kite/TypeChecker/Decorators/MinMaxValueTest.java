fc4336f187638bd737623e9fde9e8f669ffec1bd
Tudor Pop <tudor14pop@gmail.com>
2025-10-02 14:49:53 +0300

    Move resource cycles

--- src/test/java/io/kite/TypeChecker/Decorators/MinMaxValueTest.java
+++ src/test/java/io/kite/TypeChecker/Decorators/MinMaxValueTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("@miValue @maxValue")
-public class MiMaxValueTest extends CheckerTest {
+public class MinMaxValueTest extends CheckerTest {
 
 
     @Test
