5f907a2fdae024c77ddb2607f2554add0847e89b
Tudor Pop <tudor14pop@gmail.com>
2025-10-02 21:51:02 +0300

    Move resource cycles

--- src/test/java/io/kite/Runtime/Decorators/DescriptionTests.java
+++ src/test/java/io/kite/Runtime/Decorators/DescriptionTests.java
@@ -1,13 +1,6 @@
 package io.kite.Runtime.Decorators;
 
-import io.kite.Base.RuntimeTest;
-import io.kite.Frontend.Lexer.Tokenizer;
-import io.kite.Frontend.Lexical.ScopeResolver;
-import io.kite.Frontend.Parser.Parser;
-import io.kite.Runtime.Environment.Environment;
-import io.kite.Runtime.Interpreter;
 import io.kite.Runtime.Values.ResourceValue;
-import io.kite.TypeChecker.TypeChecker;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 
@@ -15,27 +8,7 @@ import org.junit.jupiter.api.Test;
  * Maybe print the description of the fields
  */
 @Log4j2
-public class DescriptionTests extends RuntimeTest {
-    private TypeChecker typeChecker;
-
-    @Override
-    protected void init() {
-        this.global = new Environment<>();
-        this.global.setName("global");
-        this.parser = new Parser();
-        this.tokenizer = new Tokenizer();
-        this.typeChecker = new TypeChecker();
-        this.scopeResolver = new ScopeResolver();
-        this.interpreter = new Interpreter(global);
-    }
-
-
-    protected Object eval(String source) {
-        program = src(source);
-        scopeResolver.resolve(program);
-        typeChecker.visit(program);
-        return interpreter.visit(program);
-    }
+public class DescriptionTests extends DecoratorTests {
 
     @Test
     void dependsOnSingleResource() {
--- src/test/java/io/kite/Runtime/Decorators/MinMaxValueTests.java
+++ src/test/java/io/kite/Runtime/Decorators/MinMaxValueTests.java
@@ -1,12 +1,5 @@
 package io.kite.Runtime.Decorators;
 
-import io.kite.Base.RuntimeTest;
-import io.kite.Frontend.Lexer.Tokenizer;
-import io.kite.Frontend.Lexical.ScopeResolver;
-import io.kite.Frontend.Parser.Parser;
-import io.kite.Runtime.Environment.Environment;
-import io.kite.Runtime.Interpreter;
-import io.kite.TypeChecker.TypeChecker;
 import io.kite.TypeChecker.TypeError;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
@@ -16,27 +9,7 @@ import org.junit.jupiter.api.Test;
  * All the tests from print but with added sensitive values.
  */
 @Log4j2
-public class MinMaxValueTests extends RuntimeTest {
-    private TypeChecker typeChecker;
-
-    @Override
-    protected void init() {
-        this.global = new Environment<>();
-        this.global.setName("global");
-        this.parser = new Parser();
-        this.tokenizer = new Tokenizer();
-        this.typeChecker = new TypeChecker();
-        this.scopeResolver = new ScopeResolver();
-        this.interpreter = new Interpreter(global);
-    }
-
-
-    protected Object eval(String source) {
-        program = src(source);
-        scopeResolver.resolve(program);
-        typeChecker.visit(program);
-        return interpreter.visit(program);
-    }
+public class MinMaxValueTests extends DecoratorTests {
 
     @Test
     void outputMinValue() {
