f65520cd9a5df4fb5d17703e379cbed2545c78b1
Tudor Pop <tudor14pop@gmail.com>
2025-10-10 13:19:50 +0300

    Add some tests for @count to check variable interpolation

--- src/test/java/io/kite/Runtime/Decorators/CountTests.java
+++ src/test/java/io/kite/Runtime/Decorators/CountTests.java
@@ -1,6 +1,8 @@
 package io.kite.Runtime.Decorators;
 
+import io.kite.Runtime.Values.SchemaValue;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -28,6 +30,44 @@ public class CountTests extends DecoratorTests {
                 
                 }
                 """);
+    }
+
+    @Test
+    void countResourceStringInterpolation() {
+        eval("""
+                schema vm { string name }
+                
+                @count(2)
+                resource vm main {
+                    name = "main-$count"
+                }
+                """);
+        var vm = (SchemaValue) global.get("vm");
+        var main0 = vm.getInstance("main[0]");
+        var main1 = vm.getInstance("main[1]");
+        Assertions.assertNotNull(main0);
+        Assertions.assertNotNull(main1);
+        Assertions.assertEquals("main-0", main0.get("name"));
+        Assertions.assertEquals("main-1", main1.get("name"));
+    }
+
+    @Test
+    void countResourceStringInterpolationAlternative() {
+        eval("""
+                schema vm { string name }
+                
+                @count(2)
+                resource vm main {
+                    name = "main-${count}"
+                }
+                """);
+        var vm = (SchemaValue) global.get("vm");
+        var main0 = vm.getInstance("main[0]");
+        var main1 = vm.getInstance("main[1]");
+        Assertions.assertNotNull(main0);
+        Assertions.assertNotNull(main1);
+        Assertions.assertEquals("main-0", main0.get("name"));
+        Assertions.assertEquals("main-1", main1.get("name"));
     }
 
     @Test
