a2c00691fa838ea440f3e3550f54bb37fa0b5e71
Tudor Pop <tudor14pop@gmail.com>
2025-10-11 12:18:36 +0300

    Skip deferred on TypeChecker

--- src/main/java/io/kite/Runtime/CycleDetectionSupport.java
+++ src/main/java/io/kite/Runtime/CycleDetectionSupport.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public interface CycleDetectionSupport {
-    default @Nullable <T> Object propertyOrDeferred(Map<String, T> environment, String name) {
+    static @Nullable <T> Object propertyOrDeferred(Map<String, T> environment, String name) {
         T t = environment.get(name);
         // if instance was not installed yet -> it will be installed later so we return a deferred object
         return Objects.requireNonNullElseGet(t, () -> new Deferred(name));
