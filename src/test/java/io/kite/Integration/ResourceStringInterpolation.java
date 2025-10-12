104b1322538149615e2909ac1e9a6ab8bcdaa9f0
Tudor Pop <tudor14pop@gmail.com>
2025-10-12 16:28:52 +0300

    Refactor base RuntimeTest

--- src/test/java/io/kite/Integration/ResourceStringInterpolation.java
+++ src/test/java/io/kite/Integration/ResourceStringInterpolation.java
@@ -1,4 +1,23 @@
 package io.kite.Integration;
 
-public class ResourceStringInterpolation extends BaseIntegrationTest{
+import org.junit.jupiter.api.Test;
+
+public class ResourceStringInterpolation extends BaseIntegrationTest {
+    @Test
+    void stringInterpolationMemberAccess() {
+        eval("""
+                        schema vm { string name; number size; }
+                
+                        resource vm main {
+                            name = "main-property"
+                            size = 1
+                        }
+                
+                        fun main() {
+                            var x = "Hello ${vm.main.name}! Your number is ${vm.main.size}"
+                            println(x)
+                        }
+                        main()
+                """);
+    }
 }
