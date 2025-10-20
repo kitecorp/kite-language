b4c842b12777fa0569750dbf4cea3d6fbe384785
Tudor Pop <tudor14pop@gmail.com>
2025-10-20 13:27:03 +0300

    Extract Interpreter into constructor

---
+++ src/main/java/io/kite/Runtime/Decorators/ExistingDecorator.java
@@ -0,0 +1,30 @@
+package io.kite.Runtime.Decorators;
+
+import io.kite.Frontend.Parse.Literals.StringLiteral;
+import io.kite.Frontend.Parser.Expressions.AnnotationDeclaration;
+import io.kite.Frontend.Parser.Expressions.ResourceStatement;
+import io.kite.Visitors.SyntaxPrinter;
+
+public class ExistingDecorator extends DecoratorInterpreter {
+    private final SyntaxPrinter printer;
+
+    public ExistingDecorator(SyntaxPrinter printer) {
+        super("existing");
+        this.printer = printer;
+    }
+
+    @Override
+    public Object execute(AnnotationDeclaration declaration) {
+        if (declaration.getTarget() instanceof ResourceStatement resource) {
+            Object value = declaration.getValue();
+            if (value == null) {
+                throw new IllegalStateException("%s Value cannot be null".formatted(printer.visit(declaration)));
+            }
+            if (value instanceof StringLiteral string) {
+                resource.setExisting(string.getValue());
+                return resource.getExisting();
+            }
+        }
+        return null;
+    }
+}
