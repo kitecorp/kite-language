507254af7f4b714d88e5c25e46dd2b0ad3775b8c
Tudor Pop <tudor14pop@gmail.com>
2025-10-22 23:08:24 +0300

    Change counted from Boxed value to value type

--- src/main/java/io/kite/TypeChecker/Types/Type.java
+++ src/main/java/io/kite/TypeChecker/Types/Type.java
@@ -1,14 +1,14 @@
 package io.kite.TypeChecker.Types;
 
 import io.kite.Frontend.Parser.Expressions.Expression;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import java.util.StringJoiner;
-
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public sealed abstract class Type extends Expression
         permits DecoratorType, FunType, ReferenceType, ValueType {
     @Getter
@@ -29,28 +29,6 @@ public sealed abstract class Type extends Expression
     }
 
     public Type() {
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (!(o instanceof Type type)) return false;
-
-        return new EqualsBuilder().appendSuper(super.equals(o)).append(getValue(), type.getValue()).isEquals();
-    }
-
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37).appendSuper(super.hashCode()).append(getValue()).toHashCode();
-    }
-
-    @Override
-    public String toString() {
-        return new StringJoiner(", ", Type.class.getSimpleName() + "[", "]")
-                .add("value='" + value + "'")
-                .add("kind=" + this.getClass().getSimpleName())
-                .toString();
     }
 
     public boolean hasValue() {
