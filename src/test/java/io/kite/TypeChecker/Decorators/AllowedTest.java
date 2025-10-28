fdaadb594aed9211afdeec8a0dc10f57492bc949
Tudor Pop <tudor14pop@gmail.com>
2025-10-28 12:34:51 +0200

    Test @allowed in component

--- src/test/java/io/kite/TypeChecker/Decorators/AllowedTest.java
+++ src/test/java/io/kite/TypeChecker/Decorators/AllowedTest.java
@@ -2,10 +2,13 @@ package io.kite.TypeChecker.Decorators;
 
 import io.kite.Base.CheckerTest;
 import io.kite.TypeChecker.TypeError;
+import io.kite.TypeChecker.Types.ValueType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static io.kite.TypeChecker.ComponentTest.assertIsComponentType;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName("@allowed decorator")
 public class AllowedTest extends CheckerTest {
@@ -271,5 +274,74 @@ public class AllowedTest extends CheckerTest {
                 """);
     }
 
+    @Test
+    void componentInputWithAllowedDecorator() {
+        var res = eval("""
+                component app {
+                    @allowed(["hello", "world"])
+                    input string something
+                }
+                """);
+
+        var appComponent = assertIsComponentType(res, "app");
+        var inputType = appComponent.lookup("something");
+        assertNotNull(inputType);
+        assertEquals(ValueType.String, inputType);
+    }
+
+    @Test
+    void componentInputWithAllowedDecoratorValidValue() {
+        var res = eval("""
+                component app {
+                    @allowed(["hello", "world"])
+                    input string something = "hello"
+                }
+                """);
+
+        var appComponent = assertIsComponentType(res, "app");
+        var inputType = appComponent.lookup("something");
+        assertEquals(ValueType.String, inputType);
+    }
+
+    @Test
+    void componentInstanceInputWithAllowedDecoratorValidValue() {
+        var res = eval("""
+                component app {
+                    @allowed(["hello", "world"])
+                    input string something
+                }
+                
+                component app prodApp {
+                    something = "hello"
+                }
+                """);
+
+        var prodAppInstance = assertIsComponentType(res, "app");
+        assertEquals("prodApp", prodAppInstance.getName());
+    }
+
+    @Test
+    void componentInputWithAllowedDecoratorWrongType() {
+        assertThrows(TypeError.class, () -> eval("""
+                component app {
+                    @allowed(["hello", "world"])
+                    input string something = 123
+                }
+                """));
+    }
+
+    @Test
+    void componentInstanceInputWithAllowedDecoratorInvalidValue() {
+        assertThrows(TypeError.class, () -> eval("""
+                component app {
+                    @allowed(["hello", "world"])
+                    input string something
+                }
+                
+                component app prodApp {
+                    something = 123
+                }
+                """));
+    }
 
 }
