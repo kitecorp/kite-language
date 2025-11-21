910f90793f4b2871f38a1161e09a2b47f0794635
Tudor Pop <tudor14pop@gmail.com>
2025-11-22 00:39:53 +0200

    Rename packages
    Signed-off-by: Tudor Pop <tudor14pop@gmail.com>

--- src/main/java/io/kite/tool/JansiHelper.java
+++
@@ -1,8 +0,0 @@
-package io.kite.tool;
-
-public class JansiHelper {
-    public static String strip(String input) {
-        // Matches any ANSI escape code: ESC [ ... letters like m, J, K, etc.
-        return input.replaceAll("\\u001B\\[[;\\d]*[ -/]*[@-~]", "").trim();
-    }
-}
