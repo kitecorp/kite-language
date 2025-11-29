c470f7a6950c82bff735eb011038c3f6f6bf95da
Tudor Pop <tudor14pop@gmail.com>
2025-11-29 17:00:51 +0200

    Add input/output/regular to schema

--- src/main/java/cloud/kitelang/utils/Reflections.java
+++ src/main/java/cloud/kitelang/utils/Reflections.java
@@ -1,12 +1,13 @@
 package cloud.kitelang.utils;
 
 import cloud.kitelang.api.annotations.Property;
+import cloud.kitelang.api.annotations.PropertyKind;
 
 import java.lang.reflect.Field;
 
 public class Reflections {
     public static boolean isImmutable(Field field) {
         Property annotation = field.getAnnotation(Property.class);
-        return annotation != null && annotation.output();
+        return annotation != null && annotation.kind() == PropertyKind.OUTPUT;
     }
 }
