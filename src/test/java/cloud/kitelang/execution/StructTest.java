16f8ae0acd473056c6f0e64dfa719b5344dd32ab
Tudor Pop <tudor14pop@gmail.com>
2025-12-11 19:02:35 +0200

    Add tests for structs as schema fields and resource creation
    Tests struct integration with schemas and resources:
    
    Schema field tests:
    - Schema can have struct type fields
    - Schema can have struct fields with defaults
    - Schema with multiple struct fields
    
    Resource creation tests:
    - Set struct using constructor (Point(10, 20))
    - Set struct using object literal ({ x: 30, y: 40 })
    - Access struct properties from resource
    - Resource inherits struct default from schema
    - Resource overrides struct default
    - Struct with partial defaults in resource
    - Resource references another resource's struct
    - Nested structs in resources
    - Multiple resources with same struct field
    - Struct arrays in resources
    - Computed struct values in resources

--- src/test/java/cloud/kitelang/execution/StructTest.java
+++ src/test/java/cloud/kitelang/execution/StructTest.java
@@ -398,4 +398,341 @@ public class StructTest extends RuntimeTest {
             assertEquals(100, result);
         }
     }
+
+    @Nested
+    @DisplayName("Struct as Schema Field")
+    class StructAsSchemaFieldTests {
+
+        @Test
+        @DisplayName("Schema can have struct type field")
+        void schemaWithStructField() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    schema Shape {
+                        Point origin
+                    }
+                    """);
+
+            var schema = interpreter.getSchema("Shape");
+            assertTrue(schema.has("origin"));
+        }
+
+        @Test
+        @DisplayName("Schema can have struct field with default")
+        void schemaWithStructFieldDefault() {
+            eval("""
+                    struct Point {
+                        number x = 0
+                        number y = 0
+                    }
+                    schema Shape {
+                        Point origin = Point()
+                    }
+                    """);
+
+            var schema = interpreter.getSchema("Shape");
+            assertTrue(schema.has("origin"));
+            var origin = schema.get("origin");
+            assertInstanceOf(StructValue.class, origin);
+            assertEquals(0, ((StructValue) origin).get("x"));
+        }
+
+        @Test
+        @DisplayName("Schema with multiple struct fields")
+        void schemaWithMultipleStructFields() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    struct Size {
+                        number width
+                        number height
+                    }
+                    schema Rectangle {
+                        Point topLeft
+                        Size dimensions
+                    }
+                    """);
+
+            var schema = interpreter.getSchema("Rectangle");
+            assertTrue(schema.has("topLeft"));
+            assertTrue(schema.has("dimensions"));
+        }
+    }
+
+    @Nested
+    @DisplayName("Struct in Resource Creation")
+    class StructInResourceCreationTests {
+
+        @Test
+        @DisplayName("Set struct value using constructor in resource")
+        void setStructUsingConstructor() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    schema Shape {
+                        Point origin
+                    }
+                    resource Shape myShape {
+                        origin = Point(10, 20)
+                    }
+                    """);
+
+            var resource = interpreter.getInstance("myShape");
+            var origin = resource.argVal("origin");
+            assertInstanceOf(StructValue.class, origin);
+            assertEquals(10, ((StructValue) origin).get("x"));
+            assertEquals(20, ((StructValue) origin).get("y"));
+        }
+
+        @Test
+        @DisplayName("Set struct value using object literal in resource")
+        void setStructUsingObjectLiteral() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    schema Shape {
+                        Point origin
+                    }
+                    resource Shape myShape {
+                        origin = { x: 30, y: 40 }
+                    }
+                    """);
+
+            var resource = interpreter.getInstance("myShape");
+            var origin = resource.argVal("origin");
+            // Object literal should be auto-coerced to struct when schema field has struct type
+            assertNotNull(origin);
+        }
+
+        @Test
+        @DisplayName("Access struct property from resource")
+        void accessStructPropertyFromResource() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    schema Shape {
+                        Point origin
+                    }
+                    resource Shape myShape {
+                        origin = Point(10, 20)
+                    }
+                    """);
+
+            var resource = interpreter.getInstance("myShape");
+            var origin = (StructValue) resource.argVal("origin");
+            assertEquals(10, origin.get("x"));
+            assertEquals(20, origin.get("y"));
+        }
+
+        @Test
+        @DisplayName("Resource inherits struct default from schema")
+        void resourceInheritsStructDefault() {
+            eval("""
+                    struct Point {
+                        number x = 0
+                        number y = 0
+                    }
+                    schema Shape {
+                        Point origin = Point(5, 5)
+                    }
+                    resource Shape myShape {
+                    }
+                    """);
+
+            var resource = interpreter.getInstance("myShape");
+            var origin = resource.argVal("origin");
+            assertInstanceOf(StructValue.class, origin);
+            assertEquals(5, ((StructValue) origin).get("x"));
+            assertEquals(5, ((StructValue) origin).get("y"));
+        }
+
+        @Test
+        @DisplayName("Resource overrides struct default from schema")
+        void resourceOverridesStructDefault() {
+            eval("""
+                    struct Point {
+                        number x = 0
+                        number y = 0
+                    }
+                    schema Shape {
+                        Point origin = Point(5, 5)
+                    }
+                    resource Shape myShape {
+                        origin = Point(100, 200)
+                    }
+                    """);
+
+            var resource = interpreter.getInstance("myShape");
+            var origin = resource.argVal("origin");
+            assertInstanceOf(StructValue.class, origin);
+            assertEquals(100, ((StructValue) origin).get("x"));
+            assertEquals(200, ((StructValue) origin).get("y"));
+        }
+
+        @Test
+        @DisplayName("Struct with partial defaults in resource")
+        void structWithPartialDefaultsInResource() {
+            eval("""
+                    struct Config {
+                        string host
+                        number port = 8080
+                    }
+                    schema Server {
+                        Config config
+                    }
+                    resource Server myServer {
+                        config = Config("localhost")
+                    }
+                    """);
+
+            var resource = interpreter.getInstance("myServer");
+            var config = resource.argVal("config");
+            assertInstanceOf(StructValue.class, config);
+            assertEquals("localhost", ((StructValue) config).get("host"));
+            assertEquals(8080, ((StructValue) config).get("port"));
+        }
+
+        @Test
+        @DisplayName("Resource references another resource struct")
+        void resourceReferencesAnotherResourceStruct() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    schema Shape {
+                        Point origin
+                        Point refPoint
+                    }
+                    resource Shape shapeA {
+                        origin = Point(10, 20)
+                    }
+                    resource Shape shapeB {
+                        origin = Point(0, 0)
+                        refPoint = shapeA.origin
+                    }
+                    """);
+
+            var shapeB = interpreter.getInstance("shapeB");
+            var refPoint = (StructValue) shapeB.argVal("refPoint");
+            assertEquals(10, refPoint.get("x"));
+            assertEquals(20, refPoint.get("y"));
+        }
+
+        @Test
+        @DisplayName("Nested struct in resource")
+        void nestedStructInResource() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    struct Rect {
+                        Point topLeft
+                        Point bottomRight
+                    }
+                    schema Canvas {
+                        Rect bounds
+                    }
+                    resource Canvas myCanvas {
+                        bounds = Rect(Point(0, 0), Point(100, 100))
+                    }
+                    """);
+
+            var resource = interpreter.getInstance("myCanvas");
+            var bounds = resource.argVal("bounds");
+            assertInstanceOf(StructValue.class, bounds);
+
+            var topLeft = ((StructValue) bounds).get("topLeft");
+            assertInstanceOf(StructValue.class, topLeft);
+            assertEquals(0, ((StructValue) topLeft).get("x"));
+        }
+
+        @Test
+        @DisplayName("Multiple resources with same struct schema field")
+        void multipleResourcesWithSameStructField() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    schema Shape {
+                        Point origin
+                    }
+                    resource Shape circle {
+                        origin = Point(10, 10)
+                    }
+                    resource Shape square {
+                        origin = Point(20, 20)
+                    }
+                    """);
+
+            var circle = interpreter.getInstance("circle");
+            var square = interpreter.getInstance("square");
+
+            var circleOrigin = (StructValue) circle.argVal("origin");
+            var squareOrigin = (StructValue) square.argVal("origin");
+
+            assertEquals(10, circleOrigin.get("x"));
+            assertEquals(20, squareOrigin.get("x"));
+        }
+
+        @Test
+        @DisplayName("Struct array in resource")
+        void structArrayInResource() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    schema Path {
+                        Point[] points
+                    }
+                    resource Path myPath {
+                        points = [Point(0, 0), Point(10, 10), Point(20, 20)]
+                    }
+                    """);
+
+            var resource = interpreter.getInstance("myPath");
+            var points = (java.util.List<?>) resource.argVal("points");
+            assertEquals(3, points.size());
+            assertInstanceOf(StructValue.class, points.get(0));
+            assertEquals(10, ((StructValue) points.get(1)).get("x"));
+        }
+
+        @Test
+        @DisplayName("Computed struct value in resource")
+        void computedStructValueInResource() {
+            eval("""
+                    struct Point {
+                        number x
+                        number y
+                    }
+                    schema Shape {
+                        Point origin
+                    }
+                    var xVal = 5 * 2
+                    var yVal = 10 + 5
+                    resource Shape myShape {
+                        origin = Point(xVal, yVal)
+                    }
+                    """);
+
+            var resource = interpreter.getInstance("myShape");
+            var origin = (StructValue) resource.argVal("origin");
+            assertEquals(10, origin.get("x"));
+            assertEquals(15, origin.get("y"));
+        }
+    }
 }
